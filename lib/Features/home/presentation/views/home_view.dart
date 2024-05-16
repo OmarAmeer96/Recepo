164675bf383d06e856e4d3209f69b1a123b3b3a0
Omar_Ameer <omar.ameer244@gmail.com>
2024-05-17 00:12:15 +0300

    Added the initial structure of the home view.

--- lib/Features/home/presentation/views/home_view.dart
+++ lib/Features/home/presentation/views/home_view.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:recepo/Core/theming/colors_manager.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -6,12 +8,101 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Home'),
-      ),
-      body: Center(
-        child: Text('Home View'),
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            title: const Text('Home'),
+            automaticallyImplyLeading: false,
+            floating: true,
+            pinned: true,
+            flexibleSpace: Container(
+              decoration: const BoxDecoration(
+                // color: ColorsManager.primaryColor,
+                gradient: LinearGradient(
+                  colors: [Colors.red, ColorsManager.primaryColor],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+              ),
+            ), // Replace with your flexibleSpace widget
+            expandedHeight: 100.h,
+          ),
+          SliverPersistentHeader(
+            delegate: _SliverAppBarDelegate(
+              minHeight: 60.0,
+              maxHeight: 100.0,
+              child: Container(
+                margin: const EdgeInsets.symmetric(horizontal: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    suffixIcon: Icon(Icons.filter_list),
+                    hintText: 'Search...',
+                    border: InputBorder.none,
+                  ),
+                ),
+              ),
+            ),
+            pinned: true,
+          ),
+          const SliverToBoxAdapter(
+            child: SizedBox(
+              height: 100.0,
+              child: Center(
+                child: Text(
+                  'Sliver To Box Adapter',
+                ),
+              ), // Replace with your widget
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(
+                    'Product Item $index',
+                  ), // Replace with your product item widget
+                );
+              },
+              childCount: 20, // Replace with your number of product items
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate({
+    required this.minHeight,
+    required this.maxHeight,
+    required this.child,
+  });
+
+  final double minHeight;
+  final double maxHeight;
+  final Widget child;
+
+  @override
+  double get minExtent => minHeight;
+  @override
+  double get maxExtent => maxHeight;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return SizedBox.expand(child: child);
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return maxHeight != oldDelegate.maxHeight ||
+        minHeight != oldDelegate.minHeight ||
+        child != oldDelegate.child;
   }
 }
