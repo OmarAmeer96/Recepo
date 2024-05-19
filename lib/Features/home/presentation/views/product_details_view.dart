80f0405d74528c3598b1e5846e819b7afaa01007
Omar_Ameer <omar.ameer244@gmail.com>
2024-05-19 03:10:00 +0300

    Product details view done.

--- lib/Features/home/presentation/views/product_details_view.dart
+++ lib/Features/home/presentation/views/product_details_view.dart
@@ -1,10 +1,13 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:recepo/Core/theming/colors_manager.dart';
 import 'package:recepo/Core/theming/font_family_helper.dart';
 import 'package:recepo/Core/theming/styles.dart';
+import 'package:recepo/Core/utils/assets.dart';
 import 'package:recepo/Core/utils/extensions.dart';
+import 'package:recepo/Core/utils/spacing.dart';
 import 'package:recepo/Core/widgets/custom_fading_widget.dart';
 import 'package:recepo/Features/home/data/models/products_model.dart';
 
@@ -77,25 +80,126 @@ class ProductDetailsView extends StatelessWidget {
               const SizedBox(height: 16),
               Text(
                 product!.title ?? '',
-                style: const TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+                style: Styles.font32BlueBold.copyWith(
+                  fontSize: 24.sp,
+                  color: ColorsManager.subPrimaryColor,
+                  fontFamily: FontFamilyHelper.bold,
                 ),
               ),
               const SizedBox(height: 16),
-              Text('Product ID: ${product!.id ?? ''}'),
-              const SizedBox(height: 8),
-              Text('Product Price: ${product!.price ?? ''}'),
-              const SizedBox(height: 8),
-              Text('Product Description: ${product!.description ?? ''}'),
-              const SizedBox(height: 8),
-              Text('Brand: ${product!.brand ?? ''}'),
-              const SizedBox(height: 8),
-              Text('Category: ${product!.category ?? ''}'),
-              const SizedBox(height: 8),
-              Text('Rating: ${product!.rating ?? ''}'),
-              const SizedBox(height: 8),
-              Text('Stock: ${product!.stock ?? ''}'),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        '\$${product!.price?.toStringAsFixed(2)}',
+                        style: Styles.onboardingContentFont.copyWith(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w600,
+                          color: ColorsManager.subPrimaryColor,
+                        ),
+                      ),
+                      horizontalSpace(20),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          SvgPicture.asset(
+                            AssetsData.star,
+                            width: 22.w,
+                          ),
+                          horizontalSpace(2),
+                          Text(
+                            product!.rating!.toString(),
+                            style: Styles.enabledTextFieldsLabelText.copyWith(
+                              fontSize: 18.sp,
+                            ),
+                          ),
+                          horizontalSpace(20),
+                          Text(
+                            'Stock: ${product!.stock ?? ''}',
+                            style: Styles.enabledTextFieldsLabelText.copyWith(
+                              fontSize: 18.sp,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  verticalSpace(16),
+                  Row(
+                    children: [
+                      Flexible(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10.w,
+                            vertical: 6.h,
+                          ),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFe2e2e2),
+                            borderRadius: BorderRadius.circular(8.r),
+                          ),
+                          child: Text(
+                            product!.brand!,
+                            style: Styles.onboardingTitleFont.copyWith(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w300,
+                              color: ColorsManager.subPrimaryColor,
+                            ),
+                            overflow: TextOverflow.fade,
+                            maxLines: 2,
+                          ),
+                        ),
+                      ),
+                      horizontalSpace(16),
+                      Flexible(
+                        child: Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 10.w,
+                            vertical: 6.h,
+                          ),
+                          decoration: BoxDecoration(
+                            color: const Color(0xFFe2e2e2),
+                            borderRadius: BorderRadius.circular(8.r),
+                          ),
+                          child: Text(
+                            product!.category!,
+                            style: Styles.onboardingTitleFont.copyWith(
+                              fontSize: 16.sp,
+                              fontWeight: FontWeight.w300,
+                              color: ColorsManager.subPrimaryColor,
+                            ),
+                            overflow: TextOverflow.fade,
+                            maxLines: 2,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  verticalSpace(16),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Description:',
+                        style: Styles.enabledTextFieldsLabelText.copyWith(
+                          fontSize: 18.sp,
+                          fontWeight: FontWeight.w600,
+                          color: ColorsManager.subPrimaryColor,
+                        ),
+                      ),
+                      verticalSpace(4),
+                      Text(
+                        product!.description ?? '',
+                        style: Styles.enabledTextFieldsLabelText.copyWith(
+                          fontSize: 16.sp,
+                        ),
+                      ),
+                    ],
+                  ),
+                  verticalSpace(16),
+                ],
+              ),
             ],
           ),
         ),
